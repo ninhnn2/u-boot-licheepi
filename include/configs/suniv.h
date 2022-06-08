2e6ac03f3c0767375ce48b79d6014ec3b6bcd3fc
nguyennhutninh <nguyennhutninh@gmail.com>
2022-06-08 14:13:07 +0700

    init source

---
+++ include/configs/suniv.h
@@ -0,0 +1,24 @@
+/*
+ * Configuration settings for new Allwinner F-series (suniv) CPU
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+/*
+ * Include common sunxi configuration where most the settings are
+ */
+#include <configs/sunxi-common.h>
+
+#define CONFIG_SYS_DFU_DATA_BUF_SIZE (512*1024)
+
+#ifdef CONFIG_VIDEO
+#define CONFIG_BMP_16BPP
+#define CONFIG_BMP_24BPP
+#define CONFIG_BMP_32BPP
+#undef CONFIG_VIDEO_LOGO
+#endif
+
+#endif /* __CONFIG_H */
